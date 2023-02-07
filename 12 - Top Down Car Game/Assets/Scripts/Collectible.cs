a90b54835439d7e6211eae6699dd10fa595acaa2
Kamidreams <ahuante447@gmail.com>
2023-02-07 11:04:48 -0800

    Creation of manager

--- 12 - Top Down Car Game/Assets/Scripts/Collectible.cs
+++ 12 - Top Down Car Game/Assets/Scripts/Collectible.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
+    [SerializeField] private int _value = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,15 @@ public class Collectible : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("I've was hit by the player!");
+            Destroy(this.gameObject);
+            //update the collectible count
+        }
     }
 }
